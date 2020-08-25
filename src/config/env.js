5984c2871dfc92f78073bc3f8a3d12389a5f5c5b
David Fall <david@madebycodera.com>
2020-08-25 12:21:57 -0700

    HS - Picsum api url added to env

--- src/config/env.js
+++ src/config/env.js
@@ -1,3 +1,4 @@
 export const JSON_PLACEHOLDER_API_URL = 'https://jsonplaceholder.typicode.com'
 export const UI_FACES_API_URL = 'https://uifaces.co/api'
 export const UI_FACES_API_TOKEN = 'A8593B36-6A9C489B-A4DA0798-3CB43376'
+export const PICSUM_API_URL = 'https://picsum.photos'
