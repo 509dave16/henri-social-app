18baf2bf75efac8ac9b3ad49e841abcbabc8defe
David Fall <david@madebycodera.com>
2020-08-25 09:52:11 -0700

    HS - Support passing of style prop to LoadingIndicator

--- src/components/widgets/LoadingIndicator.js
+++ src/components/widgets/LoadingIndicator.js
@@ -2,10 +2,10 @@ import * as React from 'react'
 import { View, ActivityIndicator, StyleSheet } from 'react-native'
 import { useTheme } from '@react-navigation/native'
 
-export default function LoadingIndicator() {
+export default function LoadingIndicator({ style }) {
   const { colors } = useTheme()
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, style]}>
       <ActivityIndicator size="large" color={colors.primary} />
     </View>
   )
